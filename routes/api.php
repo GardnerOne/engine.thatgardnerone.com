649c6f3a9db8d9fd914bc7bef4b17ab6e1761cf0
Jamie Gardner <jamie@thatgardnerone.com>
2022-02-08 17:07:47 +0000

    A user can create a project

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Project;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/projects', function () {
+    // Validate
+    // Persist
+    Project::create([
+        'title' => \request('title'),
+        'description' => \request('description'),
+    ]);
+    // Redirect
 });
---
+++ tests/Feature/ManageProjectsTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ManageProjectsTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    /** @test */
+    public function a_user_can_create_a_project()
+    {
+        $this->withoutExceptionHandling();
+
+        $attributes = [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+        ];
+
+        // Hit the /projects endpoint with the required attributes
+        $this->post('/api/projects', $attributes);
+
+        // Assert the db has been updated with the new record
+        $this->assertDatabaseHas('projects', $attributes);
+    }
+}
